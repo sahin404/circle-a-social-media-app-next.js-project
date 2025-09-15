5f73470233921b9f4006cdd4fa96336f41741667
Md. Sahin Alan <sahinraj20@gmail.com>
2025-09-15 16:08:15 +0600

    unauthorized sidebar complete

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,8 @@
 import { getUserByClerkId } from "@/actions/user.actions";
 import { currentUser } from "@clerk/nextjs/server";
 import UnAuthenticatedSidebar from "./UnAuthenticatedSidebar";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./ui/card";
+
 
 const Sidebar = async () => {
   const user = await currentUser();
@@ -10,7 +12,23 @@ const Sidebar = async () => {
 
   if (!dbUser) return <UnAuthenticatedSidebar />;
   console.log(dbUser);
-  return <div>{dbUser.name}</div>;
+  return (
+    <div>
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+          
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 };
 
 export default Sidebar;
--- src/components/UnAuthenticatedSidebar.tsx
+++ src/components/UnAuthenticatedSidebar.tsx
@@ -1,7 +1,40 @@
+import { SignInButton, SignUpButton } from "@clerk/nextjs";
+import { Button } from "./ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+import { Separator } from "./ui/separator";
 const UnAuthenticatedSidebar = () => {
   return (
-    <div>UnAuthenticatedSidebar</div>
-  )
-}
+    <div>
+      <Card>
+        <CardHeader className="text-center">
+          <CardTitle>Welcome Back!</CardTitle>
+          <CardDescription>
+            Login to access your profile and connect with others
+          </CardDescription>
+          <Separator />
+        </CardHeader>
+        <CardContent>
+          <div className="flex flex-col gap-2">
+            <SignInButton mode="modal">
+              <Button className="" variant="destructive">
+                Login{" "}
+              </Button>
+            </SignInButton>
 
-export default UnAuthenticatedSidebar
+            <SignUpButton mode="modal">
+              <Button>Sign Up</Button>
+            </SignUpButton>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default UnAuthenticatedSidebar;
