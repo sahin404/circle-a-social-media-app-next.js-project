ce2658edf89078bf0f1d91d7060b9aef2ac0e854
Md. Sahin Alan <sahinraj20@gmail.com>
2025-09-16 01:56:11 +0600

    working with after clicking follow button

--- src/components/FollowButton.tsx
+++ src/components/FollowButton.tsx
@@ -1,12 +1,40 @@
-"use client"
-const FollowButton = ({userId}:{userId:string}) => {
+"use client";
+
+import { Loader2Icon } from "lucide-react";
+import { useState } from "react";
+import { Button } from "./ui/button";
+
+const FollowButton = ({ userId }: { userId: string }) => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleFollowButton = ()=>{
+    setIsLoading(true);
+    try{
+
+    }
+    catch(err){
+
+    }
+    finally{
+        setIsLoading(false);
+    }
+
+  }
   return (
     <div>
-
-
-        
+      <Button
+        variant="secondary"
+        disabled={isLoading}
+        onClick={handleFollowButton}
+      >
+        {isLoading ? (
+          <Loader2Icon className="size-4 animate-spin"></Loader2Icon>
+        ) : (
+          "Follow"
+        )}
+      </Button>
     </div>
-  )
-}
+  );
+};
 
-export default FollowButton
+export default FollowButton;
