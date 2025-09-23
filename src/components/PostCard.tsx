f3bcb10a1e4fdc4645eca822c9dc769bed51b34a
Md. Sahin Alan <sahinraj20@gmail.com>
2025-09-23 11:29:29 +0600

    comment open and close toggole

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,21 +1,26 @@
-import { Post } from "@/generated/prisma";
+"use client";
+import { Comment, Post } from "@/generated/prisma";
 import { Card } from "./ui/card";
 import Image from "next/image";
 import { formatDistanceToNow } from "date-fns";
-import { Heart, MessageCircle, Trash } from "lucide-react";
+import { Divide, Heart, MessageCircle, Trash } from "lucide-react";
+import { Separator } from "./ui/separator";
+import { useState } from "react";
 type PostWithAuthor = Post & {
   author: {
     name: string;
     username: string;
     image: string;
-  },
-  _count:{
-    likes:number,
-    comments:number
-  }
+  };
+  _count: {
+    likes: number;
+    comments: number;
+  };
+  comments: Comment[];
 };
 
 const PostCard = ({ post }: { post: PostWithAuthor }) => {
+  const [openComments, setOpenComments] = useState(false);
   return (
     <div>
       <Card>
@@ -46,33 +51,51 @@ const PostCard = ({ post }: { post: PostWithAuthor }) => {
               <Trash height={18} />
             </div>
           </div>
-         
+
           {/* Content */}
           <div>
             {/* Text */}
             <div className="px-2 py-5">
-                <span>{post.content}</span>
+              <span>{post.content}</span>
             </div>
             {/* TODO: Image */}
-            <div>
-
-            </div>
+            <div></div>
           </div>
 
           {/* Footer */}
           <div className="flex  gap-5">
             <div className="flex items-center gap-1">
-                <Heart height={18} />
-                <span>{post._count.likes}</span>
+              <Heart height={18} />
+              <span>{post._count.likes}</span>
             </div>
 
-            <div className="flex items-center gap-1">
+            <div>
+              <button
+                className="flex items-center gap-1"
+                onClick={() => setOpenComments(!openComments)}
+              >
                 <MessageCircle height={18} />
-                <span>{post._count.comments}</span>    
+                <span>{post._count.comments}</span>
+              </button>
             </div>
           </div>
-          
 
+          {openComments && (
+            <div>
+              {/* Comment Section */}
+              <div className="my-5">
+                <Separator></Separator>
+              </div>
+              <div>
+                {/* Fetching Comment */}
+                <div>
+                  {post.comments.map((comment) => (
+                    <div></div>
+                  ))}
+                </div>
+              </div>
+            </div>
+          )}
         </div>
       </Card>
     </div>
