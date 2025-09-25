ce989a22499f7add1c66e81b46895651bd604a21
Md. Sahin Alan <sahinraj20@gmail.com>
2025-09-25 17:45:35 +0600

    fixed the proptypes error

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,7 +12,7 @@ const Navbar = async() => {
   }
   if(!user) return null;
   const dbUser = await getUserByClerkId(user.id);
-
+  if (!dbUser) return null;
   return (
     <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">
       <div className="max-w-7xl mx-auto">
