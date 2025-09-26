da4f293ca4577315270984422c80aebb1d1388e8
Md. Sahin Alan <sahinraj20@gmail.com>
2025-09-26 14:45:05 +0600

    created layout of profile page

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -1,12 +1,16 @@
 import { getUserProfileInfo } from "@/actions/profile.actions";
+import { Card } from "@/components/ui/card";
 import React from "react";
 
 const page = async ({ params }: { params: { username: string } }) => {
-  
   const userProfile = await getUserProfileInfo(params.username);
   console.log(userProfile);
 
-  return <div>{params.username}</div>;
+  return (
+    <div className="max-w-[1000px] mx-auto">
+      <Card>{params.username}</Card>
+    </div>
+  );
 };
 
 export default page;
