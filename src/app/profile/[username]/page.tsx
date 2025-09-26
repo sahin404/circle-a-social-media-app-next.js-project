6c3280a48a222494618711eb785aeca2e491f1b4
Md. Sahin Alan <sahinraj20@gmail.com>
2025-09-26 20:05:36 +0600

    showed the posts

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -12,12 +12,28 @@ const page = async ({ params }: { params: { username: string } }) => {
   return (
     <div className="max-w-[1000px] mx-auto">
       <Card>
+        {/* Header */}
         <div className="relative">
           <div>
             <CoverPhoto CI={userProfile.coverImage}></CoverPhoto>
           </div>
           <div className="absolute top-64 left-12">
             <ProfilePhoto PI={userProfile.profileImage}></ProfilePhoto>
+          </div>
+        </div>
+        {/* Content */}
+        <div className="mt-28 p-6 grid gap-6 lg:grid-cols-12">
+          {/*Left  */}
+          <div className="hidden lg:block lg:col-span-4">
+            <Card>Hello From Bangladesh</Card>
+          </div>
+          {/* Right */}
+          <div className="lg:col-span-8">
+            <Card className="p-6 space-y-6">
+            {
+              userProfile.posts.map(post=><PostCard post={post}></PostCard>)
+            }  
+            </Card>
           </div>
         </div>
       </Card>
